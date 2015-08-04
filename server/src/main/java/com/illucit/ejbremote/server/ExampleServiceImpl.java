4fba5e62415935370860b1ab068b1140817ffb9a
Pontus Ullgren <ullgren@gmail.com>
2015-08-04 22:27:37 +0200

    Added server side logging

--- server/src/main/java/com/illucit/ejbremote/server/ExampleServiceImpl.java
+++ server/src/main/java/com/illucit/ejbremote/server/ExampleServiceImpl.java
@@ -2,6 +2,7 @@ package com.illucit.ejbremote.server;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.jboss.logging.Logger;
 
 import javax.ejb.Stateless;
 
@@ -13,14 +14,17 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class ExampleServiceImpl implements ExampleService {
+        private static final Logger log = Logger.getLogger(ExampleServiceImpl.class.getName());
 
 	@Override
 	public String greet(String name) {
+                log.info("greet called with name " + name);
 		return "Hello " + name + "!";
 	}
 
 	@Override
 	public Map<Object, Object> getSystemProperties() {
+                log.info("getSystemProperties called");
 		return new HashMap<>(System.getProperties());
 	}
 
